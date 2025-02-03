e4aa824cc4cce6dcf13fbeb482795ffadce75ea4
David <duwagbale07@gmail.com>
2025-02-03 10:51:05 +0100

    Update: - Add profile image - Add metatags for better seo - Add fa-icons

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   function updateCurrentTime() {
     const currentTimeElement = document.querySelector('[data-testid="currentTimeUTC"]');
     const currentTime = new Date().toUTCString();
-    currentTimeElement.textContent = `Current UTC Time: ${currentTime}`;
+    currentTimeElement.textContent = currentTime;
   }
 
   // Update the time every second
-  setInterval(updateCurrentTime, 1000);
   updateCurrentTime();
 });

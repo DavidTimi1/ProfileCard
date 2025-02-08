18d3d1caddbe4300277064dd9422f078d2c48d32
David <duwagbale07@gmail.com>
2025-02-08 20:58:25 +0100

    Fix (config): Add vite config for baseless html

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/ProfileCard/',  // Important: this ensures Vite looks for assets relative to the root of the dist folder
+});
